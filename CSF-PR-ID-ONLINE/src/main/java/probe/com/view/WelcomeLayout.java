0f46acc96da0de169ec6a8e51d39f64e609f37c3
Yehia Farag <y.m.farag@gmail.com>
2018-11-01 13:29:43 +0100

    update link to papers fix user folder link

--- CSF-PR-ID-ONLINE/src/main/java/probe/com/view/WelcomeLayout.java
+++ CSF-PR-ID-ONLINE/src/main/java/probe/com/view/WelcomeLayout.java
@@ -92,17 +92,17 @@ public class WelcomeLayout extends VerticalLayout implements Serializable {
         rightLayout.setComponentAlignment(image1, Alignment.MIDDLE_CENTER);
         
          
-         Label para_6 = new Label("<p align='justify' Style='margin-left:40px;color:#585858;'><font size=\"2\"><a Style='color: #3b5b9b;text-decoration: none !important;' href='http://129.177.231.63/csf-pr/' target=\"_blank\">For protein quantitative data data visit CSF-PR <font size=\"1.5\">v2.0</font></a>.</font></p>");
+         Label para_6 = new Label("<p class='linkwithimg' align='justify' Style='margin-left:40px;color:#585858;'><font size=\"2\"><a class='link'style='font-size: 15px !important; color: #4d749f;font-weight: 500;' href='http://129.177.231.63/csf-pr/' target=\"_blank\"><img class='smallimgicon' src='VAADIN/themes/dario-theme/img/scatter_plot_applied.png' alt='CSF-PR v2.0'>For protein quantitative data data visit CSF-PR <font size=\"1.5\">v2.0</font></a>.</font></p>");
         para_6.setContentMode(ContentMode.HTML);
         leftLayout.addComponent(para_6);
         
 
-        Label para_3 = new Label("<p align='justify' Style='margin-left:40px;color:#585858;'><font size=\"2\">CSF-PR is being developed by the <a Style='color:#585858;' href='http://www.uib.no/rg/probe' target=\"_blank\">Proteomics Unit</a> at the<a Style='color:#585858;' href='http://www.uib.no/biomedisin/en' target=\"_blank\"> Department of Biomedicine at the University of Bergen</a>, Norway, in close collaboration with <a Style='color:#585858;' href='http://www.uib.no/en/rg/kgj-ms' target='_blank'> the Kristian Gerhard Jebsen Centre for MS Research, Haukeland University Hospital, Bergen, Norway.</font></p>");
+        Label para_3 = new Label("<p align='justify' Style='margin-left:40px;'><font size=\"2\">CSF-PR is being developed by the <a  class='link' href='http://www.uib.no/rg/probe' target=\"_blank\">Proteomics Unit</a> at the<a class='link'  href='http://www.uib.no/biomedisin/en' target=\"_blank\"> Department of Biomedicine at the University of Bergen</a>, Norway, in close collaboration with <a class='link'  href='http://www.uib.no/en/rg/kgj-ms' target='_blank'> the Kristian Gerhard Jebsen Centre for MS Research, Haukeland University Hospital, Bergen, Norway.</font></p>");
         para_3.setContentMode(ContentMode.HTML);
         leftLayout.addComponent(para_3);
         
         
-         Label para_4 = new Label("<p align='justify' Style='margin-left:40px;color:#585858;'><font size=\"2\">See also: <a Style='color:#585858;' href='http://www.mcponline.org/content/13/11/3152.full.pdf+html' target=\"_blank\">Guldbrandsen et al.: In-depth Characterization of the Cerebrospinal Fluid (CSF) Proteome Displayed Through the CSF Proteome Resource (CSF-PR). Mol Cell Proteomics. 2014</a>.</font></p>");
+         Label para_4 = new Label( "<p align='justify' Style='margin-left:40px;margin-bottom:2px;width:650px;text-align: justify;text-justify: inter-word;font-size: 13px;color: #585858;/* font-weight: bold; */line-height: 12px;'><font>Publications:</font></p><ul align='justify' Style='margin-left:16px;margin-bottom:2px;margin-top:2px;text-align: justify;text-justify: inter-word;font-size: 12px;color: #585858 !important;/* font-weight: bold; */line-height: 20px;'><li><a class='link' href='http://www.mcponline.org/content/16/2/300' target='_blank'>Guldbrandsen et al.: CSF-PR 2.0: an interactive literature guide to quantitative cerebrospinal fluid mass spectrometry data from neurodegenerative disorders. Mol Cell Proteomics. 2017 Feb;16(2):300-309.</font></li><li><a class='link' href='http://www.mcponline.org/content/13/11/3152.full.pdf+html' target='_blank'>Guldbrandsen et al.: In-depth Characterization of the Cerebrospinal Fluid (CSF) Proteome Displayed Through the CSF Proteome Resource (CSF-PR). Mol Cell Proteomics. 2014 Nov;13(11):3152-63.</a></li></ul>");
         para_4.setContentMode(ContentMode.HTML);
         leftLayout.addComponent(para_4);
         
